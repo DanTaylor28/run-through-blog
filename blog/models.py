d960c0b322bbe55e2c69fd997e4ccf35c7d2dc12
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-13 14:16:12 +0000

    Update and edit blog posts

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    title_tag = models.CharField(max_length=250, default="My Awesome Food Blog!!")
+    title_tag = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
@@ -13,6 +13,6 @@ class Post(models.Model):
         return f"{self.title} | {self.author}"
 
     def get_absolute_url(self):
-        return reverse('article_detail', args=str(self.id))
-
+        # return reverse('article_detail', args=str(self.id))
+        return reverse('home')
 
