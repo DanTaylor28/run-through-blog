bf6fb220166c584359702ef661ab66adfb6ffda1
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-13 14:59:55 +0000

    aa

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title} | {self.author}"
